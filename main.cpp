e928bd94231c1e4fbc1aa365710623027af9cc2f
Alexey <cheremisinov.ao@students.sch2.ru>
2021-09-17 17:39:10 +0300

    Questions from file

--- main.cpp
+++ main.cpp
@@ -5,16 +5,19 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian" );
-    ofstream a;
-    string b;
-    string c;
-    string d;
-    a.open("output.txt");
-    cout << "Введите ваше имя и фамилию: " << endl;
-    cin >> b >> c;
-    cout << "Какую книгу вы можете посоветовать?" << endl;
-    cin>>d;
-    a<<b<<' '<<c<<endl;
-    a<<d;
-    a.close();
+    ifstream a;
+    char b[100];
+    char c[100];
+    a.open("input.txt");
+    a.getline(b, 100);
+    cout<<b<<endl;
+    cin>>c;
+
+    char m[100];
+    char n[100];
+    ifstream j;
+    j.open("opros.txt");
+    j.getline(m, 100);
+    cout << m;
+    cin >>n;
 }
