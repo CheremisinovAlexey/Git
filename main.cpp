6499072a755469fe28753e25e0cc5cefd3f66977
Alexey <cheremisinov.ao@students.sch2.ru>
2021-09-17 19:31:45 +0300

    Choice

--- main.cpp
+++ main.cpp
@@ -5,24 +5,35 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian" );
-    ofstream a;
-    string b;
-    string c;
-    string d;
-    a.open("output.txt");
-    cout << "Введите ваше имя и фамилию: " << endl;
-    cin >> b >> c;
-    cout << "Какую книгу вы можете посоветовать?" << endl;
-    cin>>d;
-    a<<b<<' '<<c<<endl;
-    a<<d;
-    a.close();
-
-    char m[100];
-    char n[100];
-    ifstream j;
-    j.open("opros.txt");
-    j.getline(m, 100);
-    cout << m;
-    cin >>n;
+    ifstream a;
+    ofstream l;
+    int q;
+    while (q != 1 & q != 2)
+    {
+    cout << "Провести опрос или выдать сохраненные результаты?" << endl;
+    cin >> q;
+    if (q == 1)
+    {
+        char m[100];
+        char n[100];
+        ifstream j;
+        j.open("opros.txt");
+        l.open("output.txt");
+        j.getline(m, 100);
+        cout << m;
+        cin >> n;
+        l << n << endl;
+    }
+    else if (q == 2)
+    {
+        char b[100];
+        a.open("output.txt");
+        a.getline(b, 100);
+        cout << b << endl;
+    }
+    else
+    {
+        cout << "Пожалуйста, введите в ответе цифру 1 или 2" << endl;
+    }
+    }
 }
