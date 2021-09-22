f060740f4f76ec2de80febbaf6c3593948fac7d5
CheremisinovAlexey <91156840+CheremisinovAlexey@users.noreply.github.com>
2021-09-22 10:14:26 +0300

    Add files via upload
    Format changes

--- main.cpp
+++ main.cpp
@@ -1,57 +1,66 @@
 #include <iostream>
 #include <fstream>
+#include <Windows.h>
+#include <cstdlib>
+#include <string>
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "Russian" );
+    SetConsoleCP(1251); //нужна библиотека Windows.h
+    SetConsoleOutputCP(1251);
     ifstream a;
     ofstream l;
-    int q;
+    int q, x;
+    x = 0;
     while (q != 1 & q != 2)
     {
-    cout << "Провести опрос или выдать сохраненные результаты?" << endl;
-    cin >> q;
-    if (q == 1)
-    {
-        char m[100];
-        char n[100];
-        ifstream j;
-        j.open("opros.txt");
-        l.open("output.txt");
-        j.getline(m, 100);
-        cout << m;
-        cin >> n;
-        l << n << endl;
-    }
-    else if (q == 2)
-    {
-        char b[100];
-        a.open("output.txt");
-        a.getline(b, 100);
-        cout << b << endl;
-    }
-    else
-    {
-        cout << "Пожалуйста, введите в ответе цифру 1 или 2" << endl;
-    }
-    }
-    char b[100];
-    char c[100];
-    a.open("input.txt");
-    a.getline(b, 100);
-    cout<<b<<endl;
-    cin>>c;
+        cout << "Провести опрос или выдать сохраненные результаты? " << endl;
+        cin >> q;
+        if (q == 1)
+        {
+            char m[100];
+            string n;
+            string k;
+            ifstream j;
+            j.open("opros.txt");
+            l.open("output.txt", std::ios::app);
+            j.getline(m, 100);
+            cout << m << endl;
+            cin >> n;
+            cin >> k;
+            l << n << ' ' << k << endl;
 
-    char m[100];
-    char n[100];
-    ifstream j;
-    j.open("opros.txt");
-    j.getline(m, 100);
-    cout << m;
-    cin >>n;
-<<<<<<< HEAD
-=======
-
->>>>>>> Kolichestvo
+            std::string line;
+            std::ifstream in("output.txt");
+            if (in.is_open())
+            {
+                while (getline(in, line))
+                {
+                    x += 1;
+                }
+            }
+            in.close();
+        }
+        else if (q == 2)
+        {
+            char line[100];
+            std::ifstream in("output.txt");
+            if (in.is_open())
+            {
+                while (in.getline(line, 100))
+                {
+                    std::cout << line << std::endl;
+                    x += 1;
+                }
+            }
+            in.close();
+        }
+        else
+        {
+            cout << "Пожалуйста, введите в ответе цифру 1 или 2" << endl;
+        }
+    }
+    cout << "Количество прошедших опрос и количество различных книг: " << x << endl;
 }
